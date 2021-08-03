bb9668b4643779a3e2086d30c59b36e2a0af58e7
jelliottDS <57093317+jelliottDS@users.noreply.github.com>
2021-08-03 18:32:07 +0100

    updated variable means script

--- HCPC/src/variable_means.R
+++ HCPC/src/variable_means.R
@@ -1,4 +1,11 @@
-df= read.csv("./data/ffp_variable_means.csv")
+#compare annual means of each location data to mean of whole data set
 
+#ffp location data
+ffp_df= read.csv("./data/ffp_variable_means.csv")
+rownames(ffp_df)= ffp_df$city
+ffp_df= apply(ffp_df[2:7],2, function(x) (x/mean(x)*100))
 
-apply(df[2:7],2, function(x) (x/mean(x)*100))
+#lowest similarities cities data
+lowest_df= read.csv("./data/lowest_sim_variable_means.csv")
+rownames(lowest_df)= lowest_df$city
+lowest_df=apply(lowest_df[2:7],2, function(x) (x/mean(x)*100))
